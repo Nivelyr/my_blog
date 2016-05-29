488c6b5fed3f693a7552263c728b4ae5c8acabf9
Koss <nivelyr@ukr.net>
2016-05-29 23:12:37 +0300

    Repair everything

--- blog/forms.py
+++ blog/forms.py
@@ -13,9 +13,4 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False,
-                                widget=forms.Textarea)
-
+    
--- blog/models.py
+++ blog/models.py
@@ -8,53 +8,13 @@ from django.core.urlresolvers import reverse
 
 # TODO: why need to use custom manager?
 
-
 class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(
-            status='published')
-
-
-class PublishedManager(models.Manager):
-
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
-
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250) #This is the field for the post title
-    slug = models.SlugField(max_length=250, 
-                            unique_for_date='publish') #This is a field intended to be used in URLs
-    author = models.ForeignKey(User,
-                                related_name='blog_posts') #This field defines a many-to-one relationship
-    body = models.TextField() #This is the body of the post
-    publish = models.DateTimeField(default=timezone.now) #This datetime indicates when the post was published
-    created = models.DateTimeField(auto_now_add=True) #This datetime indicates when the post was created
-    updated = models.DateTimeField(auto_now=True) #This datetime indicates the last time the post has been updated
-    status = models.CharField(max_length=10,
-                                choices=STATUS_CHOICES,
-                                default='draft') #This is a field to show the status of a post
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                        args=[self.publish.year,
-                                self.publish.strftime('%m'),
-                                self.publish.strftime('%d'),
-                                self.slug])
-
-
-class Post(models.Model):
-    STATUS_CHOICES = (
-        ('draft', 'Draft'),
-        ('published', 'Published'),
-    )
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'),)
     title = models.CharField(max_length=250, null=False, blank=False)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts')
@@ -62,17 +22,12 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager()
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.strftime('%m'),
-                             self.publish.strftime('%d'),
-                             self.slug])
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
 
     class Meta:
         ordering = ('-publish',)
@@ -80,18 +35,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
-class Comment(models.Model):
-    class Meta:
-        """
-        Sorting results by the publish field in descending order by default when we query the
-        database.
-        """
-        ordering = ('-publish',)
-    
-    def __str__(self):
-        
-        return self.title
 
 class Comment(models.Model):
 
